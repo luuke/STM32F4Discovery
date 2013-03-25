6461279b1e3eb5ffbae25d0bce59ae7715479caa
Luuke <lukasz.bartosz1990@gmail.com>
2013-03-25 18:07:10 +0100

    first commit

---
+++ main.d
@@ -0,0 +1,8 @@
+main.o: main.c
+main.o: stm32f4xx.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
+main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
+main.o: system_stm32f4xx.h
---
+++ system_stm32f4xx.d
@@ -0,0 +1,8 @@
+system_stm32f4xx.o: system_stm32f4xx.c
+system_stm32f4xx.o: stm32f4xx.h
+system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
+system_stm32f4xx.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
+system_stm32f4xx.o: system_stm32f4xx.h
